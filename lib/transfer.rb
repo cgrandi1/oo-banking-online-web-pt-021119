0e29206bc3a8765895f9a5de4a0bbc78380d4b26
Carlton Dwayne <carltondgrandison@gmail.com>
2019-03-28 17:47:11 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -26,7 +26,7 @@ class Transfer
       @sender.balance -= amount
       @status = "complete"
     else @status == "rejected"
-      "Transaction rejected. Please check your account balance."
+      return "Transaction rejected. Please check your account balance."
     end 
   end 
 end
