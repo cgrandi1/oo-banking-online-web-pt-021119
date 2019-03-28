ed4d1f859f6cba3916044dade18cd784832f0966
Carlton Dwayne <carltondgrandison@gmail.com>
2019-03-28 17:34:01 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,7 +21,7 @@ class Transfer
   end 
   
   def execute_transaction 
-    if 
+    if @status = "pending"
     @receiver.balance += amount
     @sender.balance -= amount
     @status = "complete"
