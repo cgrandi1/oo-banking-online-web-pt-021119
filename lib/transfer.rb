0d79824a6a710c5e8872bef76dcacf3ff9a3013a
Carlton Dwayne <carltondgrandison@gmail.com>
2019-03-28 18:22:13 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -32,5 +32,12 @@ class Transfer
   end
   
   def reverse_transfer
+    if @status == "pending" && valid? == true && @sender.balance >= @amount
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
   end 
 end
